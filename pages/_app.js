30736ec88dcd0d1f15dfc12d057068491a57571f
huertatas <max.54.1990@gmail.com>
2023-01-12 16:36:09 +0100

    bdd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.scss'
+import "../styles/globals.scss";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={{ fontFamily: "Aeonik" }}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
